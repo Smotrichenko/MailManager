c32f52f1c0a0e97d8be802541df323e4a85cb94f
Max_Smotr <smotrichenko@yandex.ru>
2026-01-28 23:06:38 +0300

    Приложение mailings+модели

---
+++ config/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+from config import settings
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    # path("", include("mailings.urls", namespace="mailings"))
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
